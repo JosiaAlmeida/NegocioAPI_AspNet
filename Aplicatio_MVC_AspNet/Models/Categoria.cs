a99eeb68a4f657f495f3fe150049a20effe77907
Josia Almeida <josiaalmeida0001@gmail.com>
2021-03-19 09:11:55 +0100

    Criando Projecto

---
+++ Aplicatio_MVC_AspNet/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplicatio_MVC_AspNet.Models
+{
+    public class Categoria
+    {
+    }
+}
